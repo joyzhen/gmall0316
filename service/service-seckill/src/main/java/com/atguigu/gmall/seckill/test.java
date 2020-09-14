0f5f38043ef7caec588ca307f241530f278301f8
“张三” <zhangsan@163.com>
2020-09-14 16:30:49 +0800

    1.0

--- service/service-seckill/src/main/java/com/atguigu/gmall/seckill/test.java
+++ service/service-seckill/src/main/java/com/atguigu/gmall/seckill/test.java
@@ -2,6 +2,6 @@ package com.atguigu.gmall.seckill;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("4.0");
+        System.out.println(1.0);
     }
 }
